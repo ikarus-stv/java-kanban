126e64d6c759abeaf68e6d7c11d9896807657c78
ikarus <ikarus-stv@rambler.ru>
2024-06-28 01:31:04 +0300

    Нажал ctrl alt l на файле InMemoryTaskManager

--- src/managers/InMemoryTaskManager.java
+++ src/managers/InMemoryTaskManager.java
@@ -10,9 +10,9 @@ import java.util.Map;
 
 public class InMemoryTaskManager implements TaskManager {
     private int lastId = 0;
-    protected final  Map<Integer, Task> taskMap = new HashMap<>();
-    protected final  Map<Integer, Subtask> subtasksMap = new HashMap<>();
-    protected final  Map<Integer, Epic> epicsMap = new HashMap<>();
+    protected final Map<Integer, Task> taskMap = new HashMap<>();
+    protected final Map<Integer, Subtask> subtasksMap = new HashMap<>();
+    protected final Map<Integer, Epic> epicsMap = new HashMap<>();
 
     @Override
     public ArrayList<Task> getAllTasks() {
@@ -206,17 +206,17 @@ public class InMemoryTaskManager implements TaskManager {
     protected void resetLastId() {
         lastId = 0;
         for (int id : taskMap.keySet()) {
-            if (id>lastId) {
+            if (id > lastId) {
                 lastId = id;
             }
         }
         for (int id : subtasksMap.keySet()) {
-            if (id>lastId) {
+            if (id > lastId) {
                 lastId = id;
             }
         }
         for (int id : epicsMap.keySet()) {
-            if (id>lastId) {
+            if (id > lastId) {
                 lastId = id;
             }
         }
